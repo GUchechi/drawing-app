65dfc8a69ee9799dfb4bd39979ba0cab2818fde7
GUchechi <godswilluchendu7@gmail.com>
2022-08-21 03:31:35 +0100

    added mouseDown event listener

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 10
+let isPressed = true;
 let color = 'black';
+let x
+let y
 
+canvas.addEventListener("mousedown", (e) => {
+    isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
+
+    console.log(x,y);
+})
 
 function drawCircle(x, y) {
     ctx.beginPath();
