1525cb182fe8d4a5543583dd907b9f7e18c53069
GUchechi <godswilluchendu7@gmail.com>
2022-08-21 03:40:44 +0100

    worked on javascript

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
 let size = 10
-let isPressed = true;
-let color = 'black';
+let isPressed = false
+colorEl.value = 'black'
+let color = colorEl.value
 let x
 let y
 
-canvas.addEventListener("mousedown", (e) => {
-    isPressed = true;
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true
 
-    x = e.offsetX;
-    y = e.offsetY;
-
+    x = e.offsetX
+    y = e.offsetY
 })
 
-canvas.addEventListener("mouseup", (e) => {
-    isPressed = false;
+document.addEventListener('mouseup', (e) => {
+    isPressed = false
 
-    x = undefined;
-    y = undefined;
-
+    x = undefined
+    y = undefined
 })
 
-canvas.addEventListener("mouseMove", (e) => {
-    
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed) {
+        const x2 = e.offsetX
+        const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
+    }
 })
 
 function drawCircle(x, y) {
@@ -34,4 +48,39 @@ function drawCircle(x, y) {
     ctx.fill()
 }
 
-drawCircle(100,200)
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.strokeStyle = color
+    ctx.lineWidth = size * 2
+    ctx.stroke()
+}
+
+function updateSizeOnScreen() {
+    sizeEL.innerText = size
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5
+
+    if(size > 50) {
+        size = 50
+    }
+
+    updateSizeOnScreen()
+})
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5
+
+    if(size < 5) {
+        size = 5
+    }
+
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change', (e) => color = e.target.value)
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
